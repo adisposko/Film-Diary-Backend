b3fa7090b276879ac98ed2c75ba3c87a055cd3cb
Adis Posko <adis.posko@gmail.com>
2024-03-12 21:55:52 +0100

    film data database implemented on mongodb

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 Film Diary Backend
 
-Server-side ExpressJS app. Browser-side React app "Film Diary" at https://github.com/adisposko/Film-Diary
+Practice project. Server-side ExpressJS app with MongoDB database. Associated browser-side React app "Film Diary" at https://github.com/adisposko/Film-Diary
 
 Functionality: Browser-side implements a diary of watched films. Server-side receives
 query to search films from limited database of 50 films, and returns an array of
 films that correspond to the search parameters (title, director, and/or year of release).
+Film database stored on MongoDB cloud.
 
 Default local port is 8000, can be changed in app.js file (must be changed on browser-side as well).
 
--- routes/filmSearch.js
+++ routes/filmSearch.js
@@ -1,17 +1,32 @@
-const searchFilms = require('../utility/searchLogic');
+// old implementation used utility/searchLogic.js with assets/film_data.json for searching films
+// new implementation with MongoDB, film data in cloud database
 
 const express = require('express');
 const router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://film_search_readonly:QYjUJGDA5nz688U6@filmdiary.lch3av0.mongodb.net/?retryWrites=true&w=majority&appName=filmdiary';
+const client = new MongoClient(url);
+const db = client.db('film_diary');
+const filmData = db.collection('film_data')
+
 router.get('/film-search', (req, res, next) => {
     const params = new URLSearchParams(req.url.slice(13));
-    const searchResults = searchFilms(
-        params.get('title'), params.get('director'), params.get('year')
-        );
-    if (!searchResults.length) {
-        return res.status(404).json({message: 'Not found!'});
+    const query = {
+        title: {$regex: '.*' + params.get('title') + '.*', $options: 'i'},
+        director: {$regex: '.*' + params.get('director') + '.*', $options: 'i'}, 
     }
-    res.json({searchResults: searchResults});
+    const year = +params.get('year');
+    if (!!year) query.year = year;
+    
+    filmData.find(query).toArray()
+    .then((searchResults) => {
+        console.log(searchResults);
+        if (!searchResults.length) {
+            return res.status(404).json({message: 'Not found!'});
+        }
+        res.json({searchResults: searchResults});
+    });
 })
 
 module.exports = router;
